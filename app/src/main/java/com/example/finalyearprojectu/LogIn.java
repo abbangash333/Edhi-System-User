cbc51dc9f7f4472f549c7f158327a131928428a0
Aurang Zeb Khan <abbangash333@gmail.com>
2019-12-23 02:19:53 +0500

    first commit

--- app/src/main/java/com/example/finalyearprojectu/LogIn.java
+++ app/src/main/java/com/example/finalyearprojectu/LogIn.java
@@ -3,16 +3,24 @@ package com.example.finalyearprojectu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
-   private TextView dontHaveAccount;
+    private TextView dontHaveAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         dontHaveAccount = findViewById(R.id.dont_have_account);
-        Intent intent = new Intent(getApplicationContext(),sign_up.class);
-        startActivity(intent);
+        dontHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sign_up.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
