ed1747ce123655d39b22f864568bcf48e0f0f9c5
Aurang Zeb Khan <abbangash333@gmail.com>
2020-03-24 14:53:47 +0500

    Firebase Connection

--- app/src/main/java/com/example/finalyearprojectu/SplashScreen.java
+++ app/src/main/java/com/example/finalyearprojectu/SplashScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
     private final int SPLASH_TIME_OUT = 3000;
@@ -13,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(SplashScreen.this,"Connection Successfull",Toast.LENGTH_LONG).show();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
